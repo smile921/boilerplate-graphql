c9e0f1c2f9395590af5d45a49c0d8d5e8af6cc8b
frere921 <cook@smile.org>
2019-01-02 14:12:57 +0800

    try try try for fun

--- src/main/java/org/smile921/graphql/rest/GraphQLEndpoint.java
+++ src/main/java/org/smile921/graphql/rest/GraphQLEndpoint.java
@@ -5,14 +5,18 @@ import java.io.File;
 import javax.management.Query;
 import javax.servlet.annotation.WebServlet;
 
+import org.smile921.graphql.po.StarWarsData;
 import org.smile921.graphql.repo.HumanRepository;
 
+import graphql.schema.Coercing;
+import graphql.schema.GraphQLScalarType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.StaticDataFetcher;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.TypeRuntimeWiring;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
@@ -21,7 +25,7 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 		super(buildSchema());
 	}
 
-	private static GraphQLSchema buildSchema() { 
+	private static GraphQLSchema buildSchema1() { 
 		SchemaParser parser = new SchemaParser();
 		File file = new File("schema.graphqls");
 		TypeDefinitionRegistry registry = parser.parse(file);
@@ -32,5 +36,42 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 		GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(registry, runtimeWiring);
 		return graphQLSchema;
 	}
+	
+	private static GraphQLSchema buildSchema() { 
+		SchemaParser parser = new SchemaParser();
+				File file = new File("schema.graphqls");
+		TypeDefinitionRegistry registry = parser.parse(file);
+
+		RuntimeWiring runtimeWiring = buildRuntimeWiring();
+		SchemaGenerator schemaGenerator = new SchemaGenerator();
+		GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(registry, runtimeWiring);
+		return graphQLSchema;
+	}
+	
+	public static  RuntimeWiring buildRuntimeWiring() {
+	        GraphQLScalarType scalar =  null;
+			return RuntimeWiring.newRuntimeWiring()
+	                .scalar(scalar )
+	                // this uses builder function lambda syntax
+	                .type("QueryType", typeWiring -> typeWiring
+	                        .dataFetcher("hero", new StaticDataFetcher(StarWarsData.getArtoo()))
+	                        .dataFetcher("human", StarWarsData.getHumanDataFetcher())
+	                        .dataFetcher("droid", StarWarsData.getDroidDataFetcher())
+	                )
+	                .type("Human", typeWiring -> typeWiring
+	                        .dataFetcher("friends", StarWarsData.getFriendsDataFetcher())
+	                )
+	                // you can use builder syntax if you don't like the lambda syntax
+	                .type("Droid", typeWiring -> typeWiring
+	                        .dataFetcher("friends", StarWarsData.getFriendsDataFetcher())
+	                )
+	                // or full builder syntax if that takes your fancy
+	                .type(
+	                		TypeRuntimeWiring.newTypeWiring("Character")
+	                                .typeResolver(StarWarsData.getCharacterTypeResolver())
+	                                .build()
+	                )
+	                .build();
+	    }
 
 }
